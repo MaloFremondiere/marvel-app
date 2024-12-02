f258e22c75c87c35234d4a27d60de0c1fb1d3445
Malo Fremondiere <malo.fremondiere@etu.univ-poitiers.fr>
2024-12-02 17:42:22 +0100

    new date format

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import { formatDate } from '../fonction/fonction_date';
 
 export function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
-      {characters.map((character) => (
-        <li key={character.id}>
-          <Link to={`/characters/${character.id}`}>
-            {character.name}
-          </Link>
-        </li>
-      ))}
+      {characters.map((character) => {
+        
+        return (
+          <li key={character.id}>
+            <Link to={`/characters/${character.id}`}>
+            <strong>{character.name}</strong> <small>- {formatDate(character.modified)}</small>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
